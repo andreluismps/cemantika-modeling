da88d395e607c4967bed16fd7a60db0fe1658388
andreluismps <andreluismps@gmail.com>
2016-05-09 01:01:38 -0300

    End of Granularity Mismatch / Imprecision heuristic

--- org.cemantika.modeling/src/org/cemantika/testing/generator/heuristics/GranularityMismatchImprecisionHeuristic.java
+++ org.cemantika.modeling/src/org/cemantika/testing/generator/heuristics/GranularityMismatchImprecisionHeuristic.java
@@ -52,37 +52,24 @@ public class GranularityMismatchImprecisionHeuristic implements SensorDefectPatt
 		Scenario derivedScenario = Scenario.newInstance(baseScenario);
 		derivedScenario.setName(baseScenario.getName() + ": Defect at Time " +(((TimeSlot)timeslot).getId() +1) + " - " + contextSourceDefectPattern);//+ " on " + situation.getName());
 		
-		LogicalContext logicalContextToAdd = findLogicalContextWithDefectInCKTB(logicalContext, contextSourceDefectPattern);
-		TimeSlot newTimeSlot = new TimeSlot(-1);
-		int i = 0;
+		TimeSlot newTimeSlot = null;
+		int index = 0;
 		for (AbstractContext timeslotAbs : derivedScenario.getContextList()){
-			
-			if (!timeslotAbs.equals(timeslot)) {
-				i++;
+			TimeSlot ts = (TimeSlot)timeslotAbs;
+			if (!ts.equals(timeslot)) {
+				if (newTimeSlot != null){
+					ts.setId(ts.getId() + 1);
+				}else{
+					index++;
+				}
 				continue;
 			}
-			newTimeSlot = TimeSlot.newInstance((TimeSlot)timeslotAbs);
-			newTimeSlot.setId(newTimeSlot.getId()+1);
 			
-			Situation timeSlotSituation = (Situation)newTimeSlot.getContextList().get(0);
-			timeSlotSituation.setName(situation.getName() + " with " + contextSourceDefectPattern);
-			timeSlotSituation.getContextList().remove(logicalContext);
-			timeSlotSituation.getContextList().add(logicalContextToAdd);
-			break;
+			newTimeSlot = getModifiedTimeSlot(ts, situation, logicalContext, contextSourceDefectPattern);
+			index++;
 		}
 		
-		int listLastPos = derivedScenario.getContextList().size() - 1;
-		List<AbstractContext> removedList = new ArrayList<AbstractContext>();
-		
-		removedList.addAll(derivedScenario.getContextList().subList(i, listLastPos));
-		derivedScenario.getContextList().removeAll(removedList);
-		
-		for (AbstractContext timeslotAbs : removedList){
-			TimeSlot ts = (TimeSlot) timeslotAbs;
-			ts.setId(ts.getId()+1);
-		}
-		derivedScenario.getContextList().add(newTimeSlot);
-		derivedScenario.getContextList().addAll(removedList);
+		derivedScenario.getContextList().add(index, newTimeSlot);
 		
 		return derivedScenario;
 	}
@@ -90,6 +77,20 @@ public class GranularityMismatchImprecisionHeuristic implements SensorDefectPatt
 	private LogicalContext findLogicalContextWithDefectInCKTB(AbstractContext originalLogicalContext, ContextSourceDefectPattern contextSourceDefectPattern){
 		String name = originalLogicalContext.getName()  + " - " + contextSourceDefectPattern.getContextSourceName() + " - " + contextSourceDefectPattern.getContextDefectPattern();
 		return logicalContextCKTBDAO.getByName(name);
+	}
+	
+	private TimeSlot getModifiedTimeSlot(TimeSlot timeSlot, Situation situation, LogicalContext logicalContext, ContextSourceDefectPattern contextSourceDefectPattern){
+		LogicalContext logicalContextToAdd = findLogicalContextWithDefectInCKTB(logicalContext, contextSourceDefectPattern);
+		
+		TimeSlot newTimeSlot = TimeSlot.newInstance(timeSlot);
+		newTimeSlot.setId(newTimeSlot.getId()+1);
+		
+		Situation timeSlotSituation = (Situation)newTimeSlot.getContextList().get(0);
+		timeSlotSituation.setName(situation.getName() + " with " + contextSourceDefectPattern);
+		timeSlotSituation.getContextList().remove(logicalContext);
+		timeSlotSituation.getContextList().add(logicalContextToAdd);
+		
+		return newTimeSlot;
 	}
 
 }
